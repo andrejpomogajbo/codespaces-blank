f13c2d53493b0dd59f6b4fc228e0a645299ef8e8
andrejpomogajbo <andrejpomogajbo1@gmail.com>
2026-09-25 13:06:22 +0000

    initial proj

--- tests/TestJob.Api.Tests/obj/Debug/net10.0/TestJob.Api.Tests.AssemblyInfo.cs
+++ tests/TestJob.Api.Tests/obj/Debug/net10.0/TestJob.Api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestJob.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982a2d5507d0fba999094ae9e3eb4886a791577e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestJob.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestJob.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
